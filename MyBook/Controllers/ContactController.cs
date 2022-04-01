36b271e6626fc125bdee365c3f231776bc42b399
Focus <maniaclord@bk.ru>
2022-04-01 21:55:52 +0300

    contact

---
+++ MyBook/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBook.Controllers;
+
+public class ContactController : Controller
+{
+    [HttpGet]
+    public IActionResult Index() =>
+        View();
+
+    [HttpPost]
+    public IActionResult Send()
+    {
+        // logic
+        return Ok();
+    }
+}
