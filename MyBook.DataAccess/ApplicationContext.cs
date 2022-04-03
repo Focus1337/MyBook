69c3dca2835a015bfe90bdcd2501f4486b99c7c8
DanilMir <mirgayazov02@gmail.com>
2022-04-03 09:09:55 +0300

    add: DbSets for Book, Author, Subs

--- MyBook.DataAccess/ApplicationContext.cs
+++ MyBook.DataAccess/ApplicationContext.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyBook.Entity;
 
 namespace MyBook.DataAccess;
 
 
 //dotnet ef migrations add [Название комита] -s .\MyBook\ -p .\MyBook.DataAccess\
 //dotnet ef database update
-public class ApplicationContext : DbContext
+public class ApplicationContext : IdentityDbContext<User>
 {
     public ApplicationContext(DbContextOptions options)
         : base(options) { }
+    
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Subscription> Subs { get; set; }
 }
