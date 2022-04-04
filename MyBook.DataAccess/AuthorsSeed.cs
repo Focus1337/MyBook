3d7b493058ed3b4f5db5b5708fbe9b8fd752d34c
DanilMir <mirgayazov02@gmail.com>
2022-04-04 19:41:13 +0300

    refactoring: partial class for Seeds

---
+++ MyBook.DataAccess/AuthorsSeed.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBook.Entity;
+
+namespace MyBook.DataAccess;
+
+public partial class Seeds
+{
+    public static void CreateAuthors(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>().HasData(
+            new
+            {
+                Id = new Guid("320852a1-b75b-4b89-b286-873c80d11727"),
+                FullName = "Антония Сьюзен Байетт",
+                Description =
+                    " английская писательница. Автор более двух десятков книг, носитель множества почётных учёных степеней различных университетов и лауреат многочисленных литературных наград и премий.",
+                Image = Array.Empty<byte>()
+            });
+    }
+}
---
+++ MyBook.DataAccess/BooksSeed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBook.Entity;
+
+namespace MyBook.DataAccess;
+
+public partial class Seeds
+{
+    public static void CreateBooks(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().HasData(
+            new
+            {
+                Id = new Guid("3cb92c37-ec67-4720-af23-d7f4d4096109"),
+                Title = "Рагнарёк",
+                AuthorId = new Guid("320852a1-b75b-4b89-b286-873c80d11727"),
+                Description =
+                    "«Рагнарёк» – книга из серии древних мифов, переосмысленных современными писателями из разных стран",
+                Genre = "Мифы",
+                Year = 2022,
+                Price = 549m,
+                SubType = 1,
+                Image = Array.Empty<byte>(),
+            });
+    }
+}
