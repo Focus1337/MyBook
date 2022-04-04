8eec4ea54856b7403b6ce19017e80abae032e5bf
Focus <maniaclord@bk.ru>
2022-04-05 00:07:31 +0300

    book details

---
+++ MyBook/Controllers/CatalogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBook.Controllers;
+
+public class CatalogController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult BookDetails() =>
+        View();
+}
