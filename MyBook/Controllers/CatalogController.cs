1b6a80028d7ecbb8761297e330f282627a9f02f3
Focus <maniaclord@bk.ru>
2022-04-08 20:59:15 +0300

    all books and etc

--- MyBook/Controllers/CatalogController.cs
+++ MyBook/Controllers/CatalogController.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyBook.DataAccess;
 
 namespace MyBook.Controllers;
 
 public class CatalogController : Controller
 {
-    // GET
+    private readonly ApplicationContext _context;
+    
+    public CatalogController(ApplicationContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
         return View();
     }
+
+    public async Task<IActionResult> Books() =>
+        View(await _context.Books.Include(x => x.Author).ToListAsync());
 
     public IActionResult BookDetails() =>
         View();
