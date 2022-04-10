14ebe6be0c7e20281e72d764f54aab53e9eb1663
Focus <maniaclord@bk.ru>
2022-04-10 22:46:13 +0300

    Pages added. Some fixes

--- MyBook/Controllers/CatalogController.cs
+++ MyBook/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyBook.DataAccess;
 
@@ -8,19 +9,32 @@ public class CatalogController : Controller
 {
     private readonly ApplicationContext _context;
     
-    public CatalogController(ApplicationContext context)
-    {
+    public CatalogController(ApplicationContext context) => 
         _context = context;
-    }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+    [HttpGet]
+    public IActionResult Index() => 
+        View();
 
+    [HttpGet]
     public async Task<IActionResult> Books() =>
-        View(await _context.Books.Include(x => x.Author).ToListAsync());
+        View(await _context.Books.Include(a => a.Author).ToListAsync());
 
+    [HttpGet]
+    public async Task<IActionResult> FreeBooks() =>
+        View(await _context.Books
+            .Include(a => a.Author)
+            .Where(s => s.SubType == 0)
+            .ToListAsync());
+    
+    [Authorize(Roles = "UserSub, Admin")]
+    [HttpGet]
+    public async Task<IActionResult> Premium() =>
+        View(await _context.Books
+            .Include(a => a.Author)
+            .Where(s => s.SubType == 1)
+            .ToListAsync());
+    
     public IActionResult BookDetails() =>
         View();
 }
